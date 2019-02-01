476ca4fcddd2b677a2d95de928099e6a0a6b8a96
John <ibortsov14@gmail.com>
2019-02-02 01:57:55 +0300

    Fix StatusTable class
    Delete a wrong constraint that were baked by bake function.

--- src/Model/Table/StatusesTable.php
+++ src/Model/Table/StatusesTable.php
@@ -58,8 +58,7 @@ class StatusesTable extends Table
             ->scalar('name')
             ->maxLength('name', 64)
             ->requirePresence('name', 'create')
-            ->allowEmptyString('name', false)
-            ->add('name', 'unique', ['rule' => 'validateUnique', 'provider' => 'table']);
+            ->allowEmptyString('name', false);
 
         $validator
             ->scalar('classname')
@@ -68,19 +67,5 @@ class StatusesTable extends Table
             ->allowEmptyString('classname', false);
 
         return $validator;
-    }
-
-    /**
-     * Returns a rules checker object that will be used for validating
-     * application integrity.
-     *
-     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
-     * @return \Cake\ORM\RulesChecker
-     */
-    public function buildRules(RulesChecker $rules)
-    {
-        $rules->add($rules->isUnique(['name']));
-
-        return $rules;
     }
 }
